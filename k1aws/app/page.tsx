22b52ed9001fbb2b4426ff014196fc2dad4d23eb
Jmjmorelli <Jvgmorelli@gmail.com>
2026-05-16 16:09:56 -0400

    home page colors

--- k1aws/app/page.tsx
+++ k1aws/app/page.tsx
@@ -19,13 +19,13 @@ export default function Home() {
       <div>
 
       </div>
-      <div style={{ display: "flex", flexDirection: "column", marginTop: "8rem", fontSize: "25px", textAlign: "center" }}>
-        <div className="subKailenColors" style={{ fontWeight: "bold", height: "15rem", alignSelf: "flex-start", marginLeft: "10vh" }}>Me</div>
-        <div className="subKailenColors" style={{ fontWeight: "bold", height: "15rem", alignSelf: "flex-end", marginRight: "10vh" }}>Policies</div>
-        <div className="subKailenColors" style={{ fontWeight: "bold", height: "15rem", alignSelf: "flex-start", marginLeft: "10vh" }}>Pricing</div>
+      <div  style={{ display: "flex", flexDirection: "column", marginTop: "8rem", fontSize: "25px", textAlign: "center" }}>
+        <div style={{ fontWeight: "bold", height: "14rem", alignSelf: "flex-start", marginLeft: "10vh", color: "#f9d9e0" }}>Me</div>
+        <div style={{ fontWeight: "bold", height: "14rem", alignSelf: "flex-end", marginRight: "10vh", color: "#a5476f" }}>Policies</div>
+        <div className="subKailenColors" style={{ fontWeight: "bold", height: "14rem", alignSelf: "flex-start", marginLeft: "10vh" }}>Pricing</div>
       </div>
 
-      <div style={{ marginTop: "50rem" }}>testing</div>
+      {/* <div style={{ marginTop: "10rem", background: "linear-gradient(to top, #830e50, #a3496f, #c27992, #dea8b7, #f9d9e0)", height: "5rem"}}>some footer!</div> */}
     </div>
   );
 }
