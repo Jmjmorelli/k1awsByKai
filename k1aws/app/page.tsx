30c7b216d99c67de54e0724bdc44cd6e2a53d283
Jmjmorelli <Jvgmorelli@gmail.com>
2026-05-16 19:40:13 -0400

    Added font and header to home

--- k1aws/app/page.tsx
+++ k1aws/app/page.tsx
@@ -17,14 +17,32 @@ export default function Home() {
         K1aws By Kai
       </div>
       <div>
+    <div className="description" style={{color: "#f9d9e0"}}>
+      GEL X | HARD GEL | GEL POLISH | SOFT GEL  
+    </div>
+    <div className="description" style={{color: "#f9d9e0"}}>
+      {/* Don't understand these? FAQ */}
+    </div>
 
+    {/* <div className="description" style={{alignSelf: "flex-end"}}>
+wewe
+    </div> */}
       </div>
-      <div  style={{ display: "flex", flexDirection: "column", marginTop: "8rem", fontSize: "25px", textAlign: "center" }}>
+      {/* <div style={{ display: "flex", flexDirection: "column", marginTop: "8rem", fontSize: "25px", textAlign: "center", height: "auto" }}>
+        <div style={{display: "flex", flexDirection: "row"}}>
         <div style={{ fontWeight: "bold", height: "14rem", alignSelf: "flex-start", marginLeft: "10vh", color: "#f9d9e0" }}>Me</div>
+        <div className="description" style={{ height: "14rem", alignSelf: "flex-end", marginLeft: "10vh", color: "#f9d9e0" }}>Kailen Simpson</div>
+        </div>
         <div style={{ fontWeight: "bold", height: "14rem", alignSelf: "flex-end", marginRight: "10vh", color: "#a5476f" }}>Policies</div>
         <div className="subKailenColors" style={{ fontWeight: "bold", height: "14rem", alignSelf: "flex-start", marginLeft: "10vh" }}>Pricing</div>
-      </div>
+      </div> */}
 
+      <div style={{ display: "flex", flexDirection: "column", marginTop: "1rem", fontSize: "25px", textAlign: "center", marginBottom: "5rem" }}>
+
+        {/* <div>
+          <div className="button-23">book</div>
+        </div> */}
+      </div>
       {/* <div style={{ marginTop: "10rem", background: "linear-gradient(to top, #830e50, #a3496f, #c27992, #dea8b7, #f9d9e0)", height: "5rem"}}>some footer!</div> */}
     </div>
   );
