6d2cdc54ba1a7b687b19219e616bfcd5b91c9e12
Jmjmorelli <Jvgmorelli@gmail.com>
2026-05-19 16:05:09 -0400

    cards created

--- k1aws/app/page.tsx
+++ k1aws/app/page.tsx
@@ -4,7 +4,7 @@ import SideMenu from "./components/SideMenu";
 
 import leftNail from "../public/leftNails.png";
 import rightNail from "../public/rightNails.png";
-import kai from "../public/output-onlinepngtools.png";
+import kai from "../public/Untitled design (3)-modified.png";
 import { relative } from "path";
 
 export default function Home() {
@@ -22,19 +22,31 @@ export default function Home() {
         <div className="description" style={{ color: "#f9d9e0" }}>
           GEL X | HARD GEL | GEL POLISH | SOFT GEL
         </div>
-        <div style={{ justifyContent: "center", display: "flex", color: "#f9d9e0", gap: "5rem", paddingTop: "1.5rem" }}>
-          <div style={{ position: "relative", width: "100vw", marginTop: "-150px" }}>
-            <Image style={{}} src={kai} width={250} loading="eager"
+        <div style={{ justifyContent: "center", display: "flex", color: "#f9d9e0", gap: "5rem", paddingTop: "2rem" }}>
+          <div style={{ position: "relative", marginTop: "-50px" }}>
+            <Image style={{}} src={kai} width={150} loading="eager"
               height={100} alt="Face" />
           </div>
+
+          <div className="description" style={{ width: "40vw", marginLeft: "-30px", background: "transparent", color: "linear-gradient(to bottom, #830e50, #f9d9e0)" }}>
+            Quick Description here, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </div>
         </div>
         <div>
-          <div className="description" style={{ width: "30vw" }}>
-            Quick Description here, of how I will make your nails look super super pretty!
-          </div>
         </div>
-        <div style={{ display: "flex", justifyContent: "center", color: "#f9d9e0", fontSize: "30px" }}>
+        <div style={{ display: "flex", justifyContent: "center", color: "#f9d9e0", fontSize: "30px", marginTop: "2rem", fontWeight: "bold" }}>
           Policies
+        </div>
+        <div style={{display: "flex", justifyContent: "center", gap: "1rem", padding: "1rem"}}>
+          <div style={{width: "33%", textAlign: "center", border: "1px solid black", height: "10rem", fontWeight: "bold"}}>
+            Payment
+            <p className="description">deposits and tings</p>
+          </div>
+          <div style={{width: "33%", textAlign: "center", border: "1px solid black", height: "10rem", fontWeight: "bold"}}>
+            Card1
+          </div>
+          <div style={{width: "33%", textAlign: "center", border: "1px solid black", height: "10rem", fontWeight: "bold"}}>
+            Card1
+          </div>
         </div>
 
         {/* <div className="description" style={{alignSelf: "flex-end"}}>
@@ -52,7 +64,7 @@ wewe
 
       <div style={{ display: "flex", flexDirection: "column", marginTop: "1rem", fontSize: "25px", textAlign: "center", marginBottom: "5rem" }}>
 
-        {/* <div>
+        {/* <div style={{marginTop: "100rem"}}>
           <div className="button-23">book</div>
         </div> */}
       </div>
